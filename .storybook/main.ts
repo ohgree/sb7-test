e93765c8c4825ca5542af8cb0c7d714cd4fa292d
MJ Shin <ohgree.dev@gmail.com>
2023-04-21 10:09:53 +0900

    AutoDocs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,28 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
+  },
+  typescript: {
+    check: true,
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        // speeds up storybook build time
+        allowSyntheticDefaultImports: false,
+        // speeds up storybook build time
+        esModuleInterop: false,
+        // makes union prop types like variant and size appear as select controls
+        shouldExtractLiteralValuesFromEnum: true,
+        // makes string and boolean types that can be undefined appear as inputs and switches
+        shouldRemoveUndefinedFromOptional: true,
+      },
+      // Filter out third-party props from node_modules except @mui packages
+      propFilter: (prop) =>
+        prop.parent
+          ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
+          : true,
+    },
   },
 };
 export default config;
